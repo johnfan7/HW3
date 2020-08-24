8289004fa16131742c189bb7e45b975753d0d62f
John Fan <jf3287@columbia.edu>
2020-08-24 15:32:31 -0400

    i finished

--- script.js
+++ script.js
@@ -1,8 +1,164 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+
+
+
+var upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
+
+
+var specialCharacters =["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":", "@","%","$","^"];
+
+var numericCharacters = ["0","1","2","3","4","5","6","7","8","9"];
+
+  
+
+
+function getPasswordOptions() {
+
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain?')
+  );
+
+
+  if (isNaN(length) === true) {
+    alert('Password length must be provided as a number');
+    return;
+
+  }
+
+  if (length < 8) {
+    alert('Password length must be at least 8 characters');
+
+    return;
+  }
+
+  if (length > 128) {
+    alert('Password length must be less than 129 characters');
+
+    return;
+  }
+
+  var hasSpecialCharacters = confirm(
+    'click OK to confirm including special characters'
+  );
+
+  var hasNumericCharacters = confirm(
+    'click OK to confirm including numeric characters'
+  );
+
+  var hasLowerCasedCharacters = confirm(
+    'click OK to confirm including lower cased characters'
+  );
+
+  var hasUpperCasedCharacters = confirm(
+    'Click OK to confirm including uppercase characters'
+  );
+
+  if (
+    hasSpecialCharacters === false &&
+    hasNumericCharacters === false &&
+    hasUpperCasedCharacters === false &&
+    hasLowerCasedCharacters === false
+  ) {
+
+    alert('must select at least one character type');
+    return;
+  };
+
+
+  var passwordOptions = {
+    length: length,
+    hasSpecialCharacters: hasSpecialCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasLowerCasedCharacters: hasLowerCasedCharacters,
+    hasUpperCasedCharacters: hasUpperCasedCharacters
+
+  };
+
+  return passwordOptions;
+
+}
+
+
+function getRandom(arr) {
+
+  var randIndex = Math.floor(Math.random() * arr.length);
+
+  var randElement = arr[randIndex];
+
+  return randElement;
+};
+
+
+function generatePassword() {
+
+
+  var options = getPasswordOptions();
+
+  var result = [];
+
+  var possibleCharacters = [];
+
+  var guaranteedCharacters = [];
+
+  if (options.hasSpecialCharacters){
+
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteedCharacters.push(getRandom(specialCharacters));
+    
+  }
+
+
+  if (options.hasNumericCharacters){
+
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+    guaranteedCharacters.push(getRandom(numericCharacters));
+    
+  }
+
+
+  if (options.hasLowerCasedCharacters){
+
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+    guaranteedCharacters.push(getRandom(lowerCasedCharacters));
+    
+  }
+
+  if (options.hasUpperCasedCharacters){
+
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+    guaranteedCharacters.push(getRandom(upperCasedCharacters));
+    
+  }
+
+  for (var i = 0; i < options.length; i++) {
+
+    var possibleCharacter = getRandom(possibleCharacters);
+
+    result.push(possibleCharacter);
+  }
+
+
+
+  for (var i = 0; i < guaranteedCharacters.length; i++) {
+    result[i] = guaranteedCharacters[i];
+  }
+
+
+  return result.join('');
+
+}
+
+var generateBtn = document.querySelector('#generate')
 
 // Write password to the #password input
 function writePassword() {
+
+  console.log("im inside writePassword!")
+  
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
